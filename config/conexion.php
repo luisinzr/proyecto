7d0839e3b1d3427cdd0472aef5722c0a2ade3905
luisinzr <126990770+luisinzr@users.noreply.github.com>
2025-07-19 23:20:44 +0000

    Corregir datos de conexión a la base de datos

--- config/conexion.php
+++ config/conexion.php
@@ -1,5 +1,11 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "restaurante_db");
+$host = "127.0.0.1";  // Cambiado de localhost a 127.0.0.1
+$usuario = "root";
+$password = "root";   // Cambia la contraseña aquí si la tienes diferente
+$bd = "restaurante_db";
+
+$conexion = new mysqli($host, $usuario, $password, $bd);
+
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
